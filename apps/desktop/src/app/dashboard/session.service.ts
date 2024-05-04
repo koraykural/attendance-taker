acc8886b24de8767633e56d05e22b3dbb75d55d2
koray-datadive <koraykurak99@gmail.com>
2024-05-04 23:05:51 +0300

    fix code socket connection

--- apps/desktop/src/app/dashboard/session.service.ts
+++ apps/desktop/src/app/dashboard/session.service.ts
@@ -6,7 +6,16 @@ import {
   CreateSessionResponseDto,
   SessionDetails,
 } from '@interfaces/session';
-import { BehaviorSubject, filter, pairwise, startWith, takeWhile } from 'rxjs';
+import {
+  BehaviorSubject,
+  Subscription,
+  combineLatest,
+  map,
+  pairwise,
+  skipWhile,
+  startWith,
+  takeWhile,
+} from 'rxjs';
 import { NavigationEnd, Router } from '@angular/router';
 import { Socket } from 'ngx-socket-io';
 
@@ -15,7 +24,9 @@ import { Socket } from 'ngx-socket-io';
 })
 export class SessionService {
   session$ = new BehaviorSubject<SessionDetails | null>(null);
+  loadingSession$ = new BehaviorSubject<boolean>(false);
   attendenceCode$ = new BehaviorSubject<string | null>(null);
+  attendenceCodeSub: Subscription | null = null;
 
   get sessionId() {
     return this.session$.value?.id;
@@ -29,47 +40,58 @@ export class SessionService {
     this.router.events
       .pipe(startWith(new NavigationEnd(0, this.router.url, this.router.url)))
       .subscribe((event) => {
-        if (!(event instanceof NavigationEnd)) {
-          return;
-        }
-
-        if (!event.url.includes('/session/session-')) {
-          return;
-        }
-
-        const sessionId = event.url.split('/').filter((part) => part.startsWith('session-'))[0];
+        const sessionId = this.getSessionIdFromNavigationEvent(event);
 
         if (sessionId && sessionId !== this.session$.value?.id) {
-          this.getSessionDetails(sessionId).subscribe((session) => {
-            this.session$.next(session);
-          });
+          this.setSession(sessionId);
         }
       });
 
-    this.session$
-      .pipe(
-        pairwise(),
-        filter(([previous, current]) => {
-          // Only listen for codes when the session changes
-          return (
-            !!current &&
-            !current.endedAt &&
-            (!previous || previous.id !== current.id || !!previous.endedAt)
-          );
-        })
-      )
-      .subscribe(() => {
-        this.listenForCodes();
-      });
+    this.listenToSessionChanges();
   }
 
-  refreshSession() {
-    if (!this.session$.value) {
+  listenToSessionChanges() {
+    this.session$.pipe(pairwise()).subscribe(([previous, current]) => {
+      const shouldListenForCodes =
+        !!current &&
+        !current.endedAt &&
+        (!previous || previous.id !== current.id || !!previous.endedAt);
+
+      if (shouldListenForCodes) {
+        this.listenForCodes();
+      } else if (!current || !!current.endedAt) {
+        this.stopListeningForCodes();
+      }
+    });
+  }
+
+  getSessionIdFromNavigationEvent(event: unknown): string | undefined {
+    if (!(event instanceof NavigationEnd)) {
       return;
     }
 
-    this.getSessionDetails(this.session$.value.id).subscribe((session) => {
+    if (!event.url.includes('/session/session-')) {
+      return;
+    }
+
+    const sessionId = event.url.split('/').filter((part) => part.startsWith('session-'))[0];
+
+    return sessionId;
+  }
+
+  refreshSession() {
+    if (!this.session$.value || this.loadingSession$.value) {
+      return;
+    }
+
+    this.setSession(this.session$.value.id);
+  }
+
+  setSession(sessionId: string) {
+    this.loadingSession$.next(true);
+    this.getSessionDetails(sessionId).subscribe((session) => {
       this.session$.next(session);
+      this.loadingSession$.next(false);
     });
   }
 
@@ -108,18 +130,40 @@ export class SessionService {
       return;
     }
 
-    this.socket
-      .fromEvent<AttendanceCodeStreamData>('session')
-      .pipe(takeWhile((data) => data.active, true))
-      .subscribe((data) => {
-        if (data.code) {
-          this.attendenceCode$.next(data.code);
+    if (this.attendenceCodeSub) {
+      this.stopListeningForCodes();
+    }
+
+    this.attendenceCodeSub = combineLatest([
+      this.socket.fromEvent<AttendanceCodeStreamData>('session'),
+      this.session$,
+      this.loadingSession$,
+    ])
+      .pipe(
+        skipWhile(([_data, _session, loading]) => loading),
+        takeWhile(([data, session]) => {
+          return data.active && !!session && !session.endedAt;
+        }, true),
+        map(([data]) => data.code)
+      )
+      .subscribe((code) => {
+        if (code) {
+          this.attendenceCode$.next(code);
         } else {
           this.attendenceCode$.next(null);
           this.refreshSession();
         }
       });
 
-    this.socket.emit('session', { id: this.sessionId });
+    this.socket.connect();
+    this.socket.emit('session', { id: session.id });
+  }
+
+  stopListeningForCodes() {
+    if (this.attendenceCodeSub) {
+      this.attendenceCodeSub.unsubscribe();
+      this.socket.disconnect();
+      this.attendenceCode$.next(null);
+    }
   }
 }
