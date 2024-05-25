39bdf6aeb04c05ab44b7b7b052defea475fdb6f8
koray-datadive <koraykurak99@gmail.com>
2024-05-25 20:57:30 +0300

    Use redis cluster

--- apps/api/src/app/redis/redis.module.ts
+++ apps/api/src/app/redis/redis.module.ts
@@ -1,22 +1,47 @@
 import { Config } from '@api/config/config';
-import { Module, Provider } from '@nestjs/common';
-import { Redis } from 'ioredis';
+import { FactoryProvider, Module, Provider } from '@nestjs/common';
+import { Redis, Cluster } from 'ioredis';
+
+const developmentRedisFactor: FactoryProvider['useFactory'] = () => {
+  const client = new Redis({
+    port: Config.REDIS_PORT,
+    host: Config.REDIS_HOST,
+  });
+
+  client.on('connect', () => {
+    console.log('Redis connected');
+  });
+  client.on('error', (err) => {
+    console.log('Redis error', err);
+  });
+
+  return client;
+};
+
+const productionRedisFactor: FactoryProvider['useFactory'] = () => {
+  const cluster = new Cluster(
+    [
+      {
+        port: Config.REDIS_PORT,
+        host: Config.REDIS_HOST,
+      },
+    ],
+    { scaleReads: 'slave' }
+  );
+
+  cluster.on('connect', () => {
+    console.log('Redis connected');
+  });
+  cluster.on('error', (err) => {
+    console.log('Redis error', err);
+  });
+
+  return cluster;
+};
 
 const redisProvider: Provider = {
   provide: Redis,
-  useFactory: () => {
-    const client = new Redis({
-      port: Config.REDIS_PORT,
-      host: Config.REDIS_HOST,
-    });
-    client.on('connect', () => {
-      console.log('Redis connected');
-    });
-    client.on('error', (err) => {
-      console.log('Redis error', err);
-    });
-    return client;
-  },
+  useFactory: Config.isProduction ? productionRedisFactor : developmentRedisFactor,
 };
 
 @Module({
